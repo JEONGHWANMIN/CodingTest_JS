054f592568f3f4edb88da18f54bfa80650e4cfe0
JEONGHWANMIN <mimi0j0j@naver.com>
2022-05-31 18:37:52 +0900

    1Solved

--- BackJoon/recursion/2.10870.ts
+++ BackJoon/recursion/2.10870.ts
@@ -1,3 +1,12 @@
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : '../input.txt';
 let input = fs.readFileSync(filePath).toString();
+
+function solution(n: number): number {
+  if (n === 0) return 0;
+  if (n <= 1) return 1;
+
+  return solution(n - 1) + solution(n - 2);
+}
+
+console.log(solution(+input));
