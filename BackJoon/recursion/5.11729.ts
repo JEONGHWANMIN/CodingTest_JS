535a46755dbd90e8c4b0ca962606749b9fc0bd5e
JeongHwanMin <65848374+JEONGHWANMIN@users.noreply.github.com>
2022-06-05 09:03:30 +0900

    ing

--- BackJoon/recursion/5.11729.ts
+++ BackJoon/recursion/5.11729.ts
@@ -1,29 +1,22 @@
-const fs = require('fs');
-const filePath = process.platform === 'linux' ? '/dev/stdin' : '../input.txt';
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "../input.txt";
 let input = fs.readFileSync(filePath).toString();
 
 const one: any[] = [];
 const two: any[] = [];
 const three: any[] = [];
+
 for (let i = 1; i <= input; i++) {
-  one.push(i);
+	one.push(i);
 }
 
 /*
 하노이탑 
-1. 첫번쨰 one 배열에 자기 자신만 남은 경우(맨밑에 큰  벽돌)무조건 3으로 가야한다. 
-2. 
+1. 원반 갯수가 1개면 목표지점에 원반을 옮기고 종료한다.
+2. 제일 아래 큰 원반이 목표지점에 도달하면 원반 갯수는 -1 해준다. (그 원반은 이제 정렬이 끝났으므로)
 */
 // [1,2,3]
 // [3] [2] [1]
-function recuresion(num: number): any {
-  if (one.length === 1) {
-    const last = one.pop();
-    three.push(last);
-  }
-  if (two[0] > num) two.push(num);
-
-  recuresion(+input);
-}
+function hanoi(num: number): any {}
 
 export {};
