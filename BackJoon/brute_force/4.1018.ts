5b3e08b69fb8808ad32105d861c8413f6ed4720e
JEONGHWANMIN <mimi0j0j@naver.com>
2022-06-12 17:50:01 +0900

    git commit auto push

--- BackJoon/brute_force/4.1018.ts
+++ BackJoon/brute_force/4.1018.ts
@@ -5,13 +5,18 @@ let input = fs.readFileSync(filePath).toString().trim().split('\n');
 const fields = input.slice(1);
 function solution(arr: string[]): void {
   console.log(arr);
-
+  let count = 0;
   for (let i = 0; i < arr.length; i++) {
     let target = arr[i];
-    for (let j = 0; j < target.length; j++) {
-      console.log(target[j]);
+    let start = arr[i][0];
+    for (let j = 1; j < target.length - 1; j++) {
+      if (target[j] === target[j + 1]) {
+        count++;
+        j++;
+      }
     }
   }
+  console.log(count);
 }
 
 solution(fields);
