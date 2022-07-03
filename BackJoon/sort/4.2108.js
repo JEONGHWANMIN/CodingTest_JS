88a62de44e510be729f1f865045fed56e546bdc3
JEONGHWANMIN <mimi0j0j@naver.com>
2022-07-03 14:03:48 +0900

    Add 1Solved

--- BackJoon/sort/4.2108.js
+++ BackJoon/sort/4.2108.js
@@ -9,14 +9,61 @@ let input = fs
 
 let N = +input.shift();
 
-input.sort((a, b) => a - b);
+/*
+산술평균 : N개의 수들의 합을 N으로 나눈 값
+중앙값 : N개의 수들을 증가하는 순서로 나열했을 경우 그 중앙에 위치하는 값
+최빈값 : N개의 수들 중 가장 많이 나타나는 값
+범위 : N개의 수들 중 최댓값과 최솟값의 차이
+*/
 
 function solutuon(arr) {
+  arr = arr.sort((a, b) => a - b);
   let result = [];
-  let sum = arr.reduce((acc, cur) => acc + cur);
-  console.log(Math.round(sum / arr.length));
-  console.log(arr[Math.floor(arr.length / 2)]);
-  let arrCount = {};
+  // 산술평균
+  let sum = Math.round(arr.reduce((acc, cur) => acc + cur) / N);
+  if (sum === -0) {
+    sum = Math.max(0, sum);
+  }
+  result.push(sum);
+  // 중앙값
+  let idx = Math.floor(arr.length / 2);
+  result.push(arr[idx]);
+  // 최빈값
+  result.push(+mode(arr));
+  // 범위
+  result.push(arr.at(-1) - arr[0]);
+  return result;
 }
 
-solutuon(input);
+function mode(arr) {
+  let countArr = [];
+  let mode = [];
+  let arrCount = {};
+  for (let el of arr) {
+    arrCount[el] = (arrCount[el] || 0) + 1;
+  }
+  for (let key in arrCount) {
+    countArr.push([key, arrCount[key]]);
+  }
+  countArr.sort((a, b) => a[1] - b[1]);
+  // 최빈값 만 배열에 넣기
+  let max = countArr.at(-1)[1];
+  for (let el of countArr) {
+    if (el[1] === max) {
+      mode.push(el);
+    }
+  }
+  mode.sort((a, b) => a[0] - b[0]);
+  if (mode.length === 1) {
+    return mode[0][0];
+  } else {
+    return mode[1][0];
+  }
+}
+
+let str = '';
+let result = solutuon(input);
+for (let el of result) {
+  str = str + el + '\n';
+}
+console.log(str);
