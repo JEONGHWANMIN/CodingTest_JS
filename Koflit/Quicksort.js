19ba56404ac6347ec2dfb8d844c2f7daaf59a904
JEONGHWANMIN <mimi0j0j@naver.com>
2022-07-16 17:50:00 +0900

    git commit auto push

--- Koflit/Quicksort.js
+++ Koflit/Quicksort.js
@@ -1,3 +1,20 @@
+// naive solution
 const quickSort = function (arr) {
-  // TODO: 여기에 코드를 작성합니다.
+  if (arr.length <= 1) return arr;
+
+  const pivot = arr[0];
+  const left = [];
+  const right = [];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] <= pivot) left.push(arr[i]);
+    else right.push(arr[i]);
+  }
+
+  const lSorted = quickSort(left);
+  const rSorted = quickSort(right);
+  return [...lSorted, pivot, ...rSorted];
 };
+
+const quick = quickSort([1, 2, 43, 100, 21]);
+console.log(quick);
