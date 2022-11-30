1a937d92e8fcfb785ab2a86a41912ebcc9f4e9d4
JEONGHWANMIN <mimi0j0j@naver.com>
2022-11-30 18:29:39 +0900

    Feat : add 1Solved

--- BackJoon/bactracking/15650.js
+++ BackJoon/bactracking/15650.js
@@ -2,12 +2,26 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "../input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split(" ");
 
-console.log(input);
-
 function solution(num, len) {
-  console.log("Start");
+  let result = "";
   const arr = Array.from({ length: num }, (_, i) => i + 1);
-  console.log(arr);
+
+  const dfs = (k, subset) => {
+    if (subset.length === len) {
+      result = result + subset.join(" ") + "\n";
+      return;
+    }
+
+    for (let i = k; i < arr.length; i++) {
+      dfs(i + 1, [...subset, arr[i]]);
+    }
+  };
+
+  dfs(0, []);
+
+  console.log(result);
 }
 
-solution(4);
+solution(+input[0], +input[1]);
+
+// 1 2 , 1 3 1 4 2 3 2 4 3 4
