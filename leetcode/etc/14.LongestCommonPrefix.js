f23d55c55f9c85fa8e5bc5cf54aedfbdde6cbe87
JEONGHWANMIN <mimi0j0j@naver.com>
2023-02-11 23:51:25 +0900

    Feat : add 1Solved

--- leetcode/etc/14.LongestCommonPrefix.js
+++ leetcode/etc/14.LongestCommonPrefix.js
@@ -3,7 +3,24 @@
  * @return {string}
  */
 var longestCommonPrefix = function (strs) {
+  let minLength = 200;
+  for (let i = 0; i < strs.length; i++) {
+    minLength = Math.min(minLength, strs[i].length);
+  }
+
   let result = "";
-  for (let i = 0; i < strs; i++) {}
+  for (let i = 0; i < minLength; i++) {
+    let isPrefixSame = true;
+    for (let j = 0; j < strs.length - 1; j++) {
+      if (strs[j][i] !== strs[j + 1][i]) {
+        isPrefixSame = false;
+        break;
+      }
+    }
+
+    if (isPrefixSame) result = result + strs[0][i];
+    else break;
+  }
+
+  return result;
 };
-sd;
