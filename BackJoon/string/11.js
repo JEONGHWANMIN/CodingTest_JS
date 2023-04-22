7908c0c9396ea49669c0603db01a1e1033e97d58
JEONGHWANMIN <mimi0j0j@naver.com>
2023-04-22 21:39:01 +0900

    Feat : add 1Solved

--- BackJoon/string/11.js
+++ BackJoon/string/11.js
@@ -1,5 +1,43 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "../input.txt";
-let input = fs.readFileSync(filePath).toString().trim();
+let input = fs.readFileSync(filePath).toString().split("\n");
 
-console.log(input.length);
+const grades = {
+  "A+": 4.5,
+  A0: 4.0,
+  "B+": 3.5,
+  B0: 3.0,
+  "C+": 2.5,
+  C0: 2.0,
+  "D+": 1.5,
+  D0: 1.0,
+  F: 0.0,
+};
+
+const result = input.reduce(
+  (pre, cur, i, arr) => {
+    const [, gradeNum, subNum] = cur.trim().split(/\s+/);
+
+    if (subNum === "P") {
+      if (i === arr.length - 1) return pre.totalSubject / pre.totalGrade;
+      return pre;
+    }
+
+    if (i === arr.length - 1) return pre.totalSubject / pre.totalGrade;
+
+    const totalGrade = pre.totalGrade + Number(gradeNum);
+    const totalSubject =
+      pre.totalSubject + Number(gradeNum) * Number(grades[subNum]);
+
+    return {
+      totalGrade,
+      totalSubject,
+    };
+  },
+  {
+    totalGrade: 0,
+    totalSubject: 0,
+  }
+);
+
+console.log(result.toFixed(6));
