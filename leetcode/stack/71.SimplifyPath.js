b6fe3aaedf6736b2240d5a4af210613f77248188
HwanMin <65848374+JEONGHWANMIN@users.noreply.github.com>
2023-10-05 00:09:37 +0900

    Update 71.SimplifyPath.js

--- leetcode/stack/71.SimplifyPath.js
+++ leetcode/stack/71.SimplifyPath.js
@@ -3,5 +3,20 @@
  * @return {string}
  */
 var simplifyPath = function(path) {
-    
+    const stack = [];
+
+    const paths = path.split("/");
+
+    const continueCase = path === '/' || path === '' || path === '.'
+
+    for (let path of paths) {
+        if (continueCase) continue;
+        if (path === '..') {
+            stack.pop();
+            continue;
+        }
+        stack.push(path)
+    }
+
+    return "/"+stack.join("/")
 };
