a3a8af2c56bb0ff070777e9c93d920dde20bee04
JEONGHWANMIN <mimi0j0j@naver.com>
2023-11-08 17:54:27 +0900

    Feat : 기능개발 문제 추가

--- programmers/기능개발.js
+++ programmers/기능개발.js
@@ -1,28 +1,27 @@
 function solution(progresses, speeds) {
-    const result = [];
-    
-    const que = [...progresses];
-    const que_speeds = [...speeds];
-    
-    while (que.length) {
-        let count = 0;
-        for (let i = 0; i < que.length; i++) {
-            que[i] = que[i] + que_speeds[i];
-        }
-        
-        if (que[0] >= 100) {
-            let i = 0;
-            while (i < que.length && que[i] >= 100) {
-                que.shift();
-                que_speeds.shift();
-                count++;
-            }
-        }
-        
-        if (count > 0) {
-            result.push(count);
-        }
+  const result = [];
+
+  const que = [...progresses];
+  const que_speeds = [...speeds];
+
+  while (que.length) {
+    let count = 0;
+    for (let i = 0; i < que.length; i++) {
+      que[i] = que[i] + que_speeds[i];
     }
-    
-    return result;
+
+    if (que[0] >= 100) {
+      while (que[0] >= 100) {
+        que.shift();
+        que_speeds.shift();
+        count++;
+      }
+    }
+
+    if (count > 0) {
+      result.push(count);
+    }
+  }
+
+  return result;
 }
