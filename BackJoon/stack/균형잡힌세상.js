9d05ded7b955619aec6ae1dc1337120625c19337
JEONGHWANMIN <mimi0j0j@naver.com>
2023-11-10 00:07:40 +0900

    Feat : 균형잡힌세상 문제풀이

--- BackJoon/stack/균형잡힌세상.js
+++ BackJoon/stack/균형잡힌세상.js
@@ -1,42 +1,45 @@
 const fs = require("fs");
-const filePath = process.platform === "linux" ? "/dev/stdin" : "../Input.txt";
-
-const input = `So when I die (the [first] I will see in (heaven) is a score list).
-[ first in ] ( first out ).
-Half Moon tonight (At least it is better than no Moon at all].
-A rope may form )( a trail in a maze.
-Help( I[m being held prisoner in a fortune cookie factory)].
-([ (([( [ ] ) ( ) (( ))] )) ]).
- .
-.`;
-
-console.log(input);
+const filePath = process.platform === "linux" ? "/dev/stdin" : "../input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
 function solution(array) {
   const result = [];
   for (let i = 0; i < array.length; i++) {
-    if (array[i] === ".") return result.join("\n");
+    if (array[i] === ".") {
+      break;
+    } else {
+      result.push(isValid(array[i]) ? "yes" : "no");
+    }
   }
+
+  return result.join("\n");
 }
 
+const opener = ["(", "{", "["];
+const closer = [")", "}", "]"];
+const closerMap = {
+  "(": ")",
+  "{": "}",
+  "[": "]",
+};
+
 function isValid(str) {
-  const opener = ["(", "{", "["];
-  const closer = [")", "}", "]"];
-  const closerMap = {
-    ")": "(",
-    "}": "{",
-    "]": "[",
-  };
   const stack = [];
 
   for (let i = 0; i < str.length; i++) {
     if (opener.includes(str[i])) {
       stack.push(str[i]);
     } else if (closer.includes(str[i])) {
-      if (stack.at(-1)) {
+      if (closerMap[stack[stack.length - 1]] === str[i]) {
+        stack.pop();
+      } else {
+        return false;
       }
     }
   }
+
+  return stack.length === 0;
 }
 
-solution(input.split("\n"));
+const result = solution(input);
+console.log(result);
