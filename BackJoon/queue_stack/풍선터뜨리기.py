ae2ace014568b8e227bbef1aa7438fe0cbaf0d49
JEONGHWANMIN <mimi0j0j@naver.com>
2023-11-15 17:50:22 +0900

    git commit auto push

--- BackJoon/queue_stack/풍선터뜨리기.py
+++ BackJoon/queue_stack/풍선터뜨리기.py
@@ -1,6 +1,13 @@
+import sys
 from collections import deque
+input = sys.stdin.readline
+
+n = int(input())
+q = deque(enumerate(map(int, input().split())))
 
 def solution(n, input_papers):
+    print(n)
+    print(input_papers)
     papers = list(map(int, input_papers.split()))
     que = deque(range(1, n + 1))
 
@@ -28,5 +35,4 @@ def solution(n, input_papers):
 
     return " ".join(map(str, result))
 
-
-
+print(solution(n,q))
