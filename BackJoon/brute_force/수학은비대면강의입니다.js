20411b6035d9471a55ca507aa80f9e130d6040fc
JEONGHWANMIN <ghksals09041@gmail.com>
2023-11-23 23:48:20 +0900

    Feat : 수학은비대면강의입니다 문제풀이

---
+++ BackJoon/brute_force/수학은비대면강의입니다.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "../input.txt";
+let input = fs.readFileSync(filePath).toString().split(" ");
+
+function solution(input) {
+  let answer_x = -1;
+  let answer_y = -1;
+  const [a, b, c, d, e, f] = input.map(Number);
+
+  for (let x = -999; x <= 999; x++) {
+    let top_x = a * x;
+    let bottom_x = d * x;
+
+    for (let y = -999; y <= 999; y++) {
+      let top_y = b * y;
+      let bottom_y = e * y;
+
+      if (top_x + top_y === c && bottom_x + bottom_y === f) {
+        answer_x = x;
+        answer_y = y;
+        break;
+      }
+    }
+  }
+
+  return `${answer_x} ${answer_y}`;
+}
+
+console.log(solution(input));
