c7c84276c722cc16471d303f9067df48197ee407
JEONGHWANMIN <ghksals09041@gmail.com>
2023-11-28 23:59:52 +0900

    Feat : 바구니바꾸기 문제풀이 추가

--- BackJoon/1_array/바구니바꾸기.js
+++ BackJoon/1_array/바구니바꾸기.js
@@ -1,7 +1,28 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "../input.txt";
-let input = fs.readFileSync(filePath).toString().trim().split("\n");
+let input = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n")
+  .map((str) => str.replace("\r", ""));
 
-function solution() {}
+const [N, M] = input.shift().split(" ").map(Number);
 
-console.log(solution());
+function solution(n, arr) {
+  let buckets = Array.from({ length: n }, (_, idx) => idx + 1);
+
+  for (let sortArr of arr) {
+    const [i, j] = sortArr.split(" ").map(Number);
+
+    const leftArr = buckets.slice(0, i - 1);
+    const rightArr = buckets.slice(j);
+    const reversedMidArr = buckets.slice(i - 1, j).reverse();
+
+    buckets = [...leftArr, ...reversedMidArr, ...rightArr];
+  }
+
+  return buckets.join(" ");
+}
+
+console.log(solution(N, input));
