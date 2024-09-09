fd88757628c67c29a0ea4e509dbf7610f533d45e
JEONGHWANMIN <mimi0j0j@naver.com>
2024-09-09 23:49:36 +0900

    Feat: 문제풀이 추가

--- leetcode/Arrays_hashing/53.MaximumSubarray.js
+++ leetcode/Arrays_hashing/53.MaximumSubarray.js
@@ -15,4 +15,22 @@ var maxSubArray = function (nums) {
 };
 
 
-console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArrayList = function (nums) {
+  let maxSum = Math.max(...nums);
+  let curSum = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    curSum = Math.max(curSum + nums[i], nums[i]);
+    maxSum = Math.max(maxSum, curSum);
+  }
+
+  return maxSum
+};
+
+
+// console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
+console.log(maxSubArrayList([-2,1,-3,4,-1,2,1,-5,4]))
