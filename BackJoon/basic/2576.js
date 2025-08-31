064f514ea092da54074099d319b36e6214863b99
JEONGHWANMIN <mimi0j0j@naver.com>
2025-08-31 19:03:56 +0900

    test

--- BackJoon/basic/2576.js
+++ BackJoon/basic/2576.js
@@ -1,4 +1,4 @@
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : '../input.txt';
-let input = fs.readFileSync(filePath).toString().trim().split('\n');
+let input2 = fs.readFileSync(filePath).toString().trim().split('\n');
 
