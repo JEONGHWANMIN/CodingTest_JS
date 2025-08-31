3716785faea727ae88be077537991ebd71ce96ef
JEONGHWANMIN <mimi0j0j@naver.com>
2025-08-31 20:01:45 +0900

    문제풀이

--- BackJoon/basic/2576.js
+++ BackJoon/basic/2576.js
@@ -1,4 +1,21 @@
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : '../input.txt';
-let input2 = fs.readFileSync(filePath).toString().trim().split('\n');
+let input = fs.readFileSync(filePath).toString().trim().split('\n').map(line => line.replace('\r', ''));
 
+const nums = input.map(Number);
+
+function solution(nums) {
+    const odds = []
+
+    for (const num of nums) {
+        if (num % 2 !== 0) odds.push(num);
+    }
+
+    if (odds.length === 0) return -1;
+
+    const sum = odds.reduce((acc, cur) => acc + cur, 0);
+    const min = Math.min(...odds)
+    return `${sum}\n${min}`
+}
+
+console.log(solution(nums));
