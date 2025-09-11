860840bc9dd55e5d76413c86e8079dbccf9a48fd
JEONGHWANMIN <mimi0j0j@naver.com>
2025-09-12 00:54:40 +0900

    문제풀이 추가

--- BackJoon/stack/2493.js
+++ BackJoon/stack/2493.js
@@ -3,8 +3,29 @@ const filePath = process.platform === 'linux' ? '/dev/stdin' : '../input.txt';
 let input = fs.readFileSync(filePath).toString().trim().split('\n').map((line) => line.replace('\r', ''));
 
 const n = Number(input.shift())
+const arr = input[0].split(" ").map(Number)
 
-function solution() {
+function solution(n, arr) {
+    const result = []
+
+    const stack = [{num: 0, index: 0}]
+
+    for (let i = 0; i < arr.length; i++) {
+        const curNum = arr[i]
+
+        while (stack.length && stack[stack.length - 1].num < curNum) {
+            stack.pop()
+        }
+
+        if (stack.length && stack[stack.length -1].num > curNum) {
+            result.push(stack[stack.length -1].index + 1) ;
+        } else {
+            result.push(0);
+        }
+        stack.push({num: curNum, index: i})
+    }
+
+    return result.join(" ")
 }
 
-console.log(solution(n, input));
+console.log(solution(n, arr));
