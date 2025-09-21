4d24f056d7dc10c06c6a2156fffe984fc55fa5c7
JEONGHWANMIN <mimi0j0j@naver.com>
2025-09-21 23:50:52 +0900

    문제풀이 추가

--- BackJoon/set_map/17219.js
+++ BackJoon/set_map/17219.js
@@ -7,12 +7,24 @@ let input = fs
     .split("\n")
     .map((line) => line.replace("\r", ""));
 
+const [n, m] = input[0].split(" ").map(Number);
 
-const [n, m] = input.shift().split(" ").map(Number);
-console.log(n,m)
+function solution(n, m, arr) {
+    const result = [];
+    const siteMap = new Map();
 
-function solution() {
+    // 인덱스를 사용해서 순차적으로 접근
+    for (let i = 1; i <= n; i++) {
+        const [site, password] = arr[i].split(" ");
+        siteMap.set(site, password);
+    }
 
+    for (let i = n + 1; i <= n + m; i++) {
+        const site = arr[i];
+        result.push(siteMap.get(site));
+    }
+
+    return result.join("\n");
 }
 
-console.log(solution());
+console.log(solution(n, m, input));
