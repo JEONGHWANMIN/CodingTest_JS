d07d0f21fcf296ce5b0b51eff7a768fd079a4c1c
JEONGHWANMIN <mimi0j0j@naver.com>
2025-09-23 00:15:21 +0900

    문제풀이 추가

--- BackJoon/set_map/16165.js
+++ BackJoon/set_map/16165.js
@@ -8,13 +8,46 @@ let input = fs
     .map((line) => line.replace("\r", ""));
 
 
-console.log(input);
 
 const [n, m] = input.shift().split(" ").map(Number);
 
-function solution() {
+function solution(n, m, arr = []) {
+    const result = [];
 
+    const idols = {}
+    const idolGroup = {}
+
+    let i = 0;
+    let groupIdx = 0;
+    while (i < n) {
+        let startGroupIdx = groupIdx;
+        const group = arr[groupIdx];
+        const people = Number(arr[groupIdx + 1]);
+        groupIdx = groupIdx + people + 2
+        idols[group] = []
+
+        for (let j = startGroupIdx + 2; j < groupIdx; j++) {
+            idolGroup[arr[j]] = group
+            idols[group].push(arr[j])
+        }
+        i++
+    }
+
+    for (let i = groupIdx; i < arr.length - 1; i++) {
+        const idol = arr[i]
+        const num = Number(arr[++i])
+
+        if (num === 0) {
+            result.push(...idols[idol].sort());
+        } else {
+            const groupName = idolGroup[idol]
+            result.push(groupName)
+        }
+    }
+
+
+    return result.join("\n")
 }
 
 
-console.log(solution());
+console.log(solution(n, m , input));
