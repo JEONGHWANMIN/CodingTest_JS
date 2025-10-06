a257f7c0c52da1d6c231cf1aced202c7a26f546c
JEONGHWANMIN <mimi0j0j@naver.com>
2025-10-06 16:41:41 +0900

    Feat: 문제풀이
    Signed-off-by: JEONGHWANMIN <mimi0j0j@naver.com>

--- BackJoon/set_map/19483.js
+++ BackJoon/set_map/19483.js
@@ -11,45 +11,41 @@ const [S, E, Q] = input.shift().split(" ")
 const arr = input
 
 function solution(s, e, q, chats) {
-  const checkEnter = (startTime, chatTime) => {
-    const [sh, sm] = startTime.split(":")
-    const [ch, cm] = chatTime.split(":")
-
-    const start = new Date().setHours(sh, sm)
-    const chat = new Date().setHours(ch, cm)
-
-    return start >= chat
+  // 시간을 분 단위로 변환하는 함수
+  const timeToMinutes = (time) => {
+    const [h, m] = time.split(":").map(Number);
+    return h * 60 + m;
   }
 
-  const checkExit = (endTime, quitTime, chatTime) => {
-    const [eh, em] = endTime.split(":")
-    const [qh, qm] = quitTime.split(":")
-    const [ch, cm] = chatTime.split(":")
-
-    const end = new Date().setHours(eh, em)
-    const quit = new Date().setHours(qh, qm)
-    const chat = new Date().setHours(ch, cm)
-
-    return chat >= end && chat <= quit
-  }
+  const startMin = timeToMinutes(s);
+  const endMin = timeToMinutes(e);
+  const quitMin = timeToMinutes(q);
 
   const enterSet = new Set();
   const exitSet = new Set();
 
   for (let chat of chats) {
-    const [time, nickname] = chat.split(" ")
+    const [time, nickname] = chat.split(" ");
+    const chatMin = timeToMinutes(time);
 
-    if (checkEnter(s, time)) enterSet.add(nickname)
-    if (checkExit(e, q, time)) exitSet.add(nickname)
+    // 시작 시간 이전 또는 같으면 입장
+    if (chatMin <= startMin) {
+      enterSet.add(nickname);
+    }
+
+    // 종료 시간과 퇴장 마감 시간 사이면 퇴장
+    if (chatMin >= endMin && chatMin <= quitMin) {
+      exitSet.add(nickname);
+    }
   }
 
+  // 입장과 퇴장 모두 한 사람 카운트
   let result = 0;
-  [...enterSet.values()].forEach((nickname) => {
-    if (exitSet.has(nickname)) result++
-  })
+  for (let nickname of enterSet) {
+    if (exitSet.has(nickname)) result++;
+  }
 
-  return result
+  return result;
 }
-
 
 console.log(solution(S, E, Q, arr));
